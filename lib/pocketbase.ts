7f1f8a02af74960893e4e7c6b07e821a21060bde
Sean Norwood <norwood.sean@gmail.com>
2022-10-01 17:38:14 -0500

    Add pocketbase url as env

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,3 +1,3 @@
 import Pocketbase from "pocketbase";
 
-export const client = new Pocketbase("http://127.0.0.1:8090");
+export const client = new Pocketbase(process.env.POCKETBASE_URL || "http://localhost:8090");
